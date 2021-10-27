ac4cd74429ff058e522e955ff32bc616cb50a76c
AlekseyE999 <79412127+AlekseyE999@users.noreply.github.com>
2021-10-27 16:14:11 +0300

    Add Table

--- laba2/laba2.cpp
+++ laba2/laba2.cpp
@@ -1,5 +1,6 @@
 #include <Windows.h>
 #include <stdlib.h>
+#include <commctrl.h>
 #include <string>
 #include <vector>
 
@@ -7,12 +8,24 @@ using namespace std;
 
 #define MAX_COL 10
 #define MAX_ROW 10
+#define PADDING 20
+
+int cols = 5, rows = 5;
 
 const wchar_t CLASS_NAME[] = L"MyWindowClass";
 
 RECT MainRect;
 
 LRESULT CALLBACK WindProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp);
+void CreateTable(HDC hdc, int tableWidth, int TextBlockHeight, int rowsCount, int colCount);
+
+vector<vector<string>> stringMatrix = {
+	{"ads", "dsfsd", "sdf", "ds", "fdsa"},
+	{"ads", "dsfsd", "sdf", "ds", "wryi"},
+	{"ads", "dkkkkkkkkkkkkkyghjiuygvhjgiyufghi7ygudasdasdasdsadd", "sdf", "ds", "fdsa"},
+	{"ads", "dkkkkkkkkkkkkkyghjiuygvhjgiyufghi7ygudasdasdasdsadd", "sdf", "ds", "fdsa"},
+	{"ads", "dsfsd", "sdf", "ds", "fdsa"}
+};
 
 int WINAPI wWinMain(HINSTANCE hInst, HINSTANCE hPrevInst, PWSTR args, int nCmdShow)
 {
@@ -57,6 +70,9 @@ LRESULT CALLBACK WindProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
 		hCmpDC = CreateCompatibleDC(hdc);
 		bitmap = CreateCompatibleBitmap(hdc, MainRect.right - MainRect.left, MainRect.bottom - MainRect.top);
 		SelectObject(hCmpDC, bitmap);
+		TEXTMETRIC textMetric;
+		GetTextMetrics(hCmpDC, &textMetric);
+		int BlockHeight = 2 * textMetric.tmHeight;
 		LOGBRUSH br;
 		br.lbStyle = BS_SOLID;
 		br.lbColor = 0xEECCCC;
@@ -64,6 +80,7 @@ LRESULT CALLBACK WindProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
 		brush = CreateBrushIndirect(&br);
 		FillRect(hCmpDC, &MainRect, brush);
 		DeleteObject(brush);
+		CreateTable(hCmpDC, MainRect.right - MainRect.left, BlockHeight, rows, cols);
 		SetStretchBltMode(hdc, COLORONCOLOR);
 		BitBlt(hdc, 0, 0, MainRect.right - MainRect.left, MainRect.bottom - MainRect.top, hCmpDC, 0, 0, SRCCOPY);;
 		DeleteDC(hCmpDC);
@@ -71,19 +88,54 @@ LRESULT CALLBACK WindProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
 		break;
 	}
 	case WM_DESTROY:
-	{
 		PostQuitMessage(0);
 		break;
-	}
-
 	case WM_SIZE:
-		GetClientRect(hwnd, &MainRect);
-		break;
-	case WM_ERASEBKGND:
-		return 1;
+		InvalidateRect(hwnd, nullptr, TRUE);
 		break;
 	default:
 		return DefWindowProc(hwnd, msg, wp, lp);
 	}
 	return 0;
+}
+
+string GetLongestString(vector<string> row, size_t startIndex)
+{
+	if (startIndex >= row.size())
+	{
+		return "";
+	}
+	string longestString = row[startIndex];
+	for (size_t j = startIndex; j < startIndex + cols; j++)
+	{
+		if (row[j].length() > longestString.length())
+			longestString = row[j];
+	}
+
+	return longestString;
+}
+
+void CreateTable(HDC hdc, int tableWidth, int textBlockHeight, int rowsCount, int colCount)
+{
+	int TextBlockWidth = (MainRect.right - MainRect.left) / colCount;
+	int totalHeight = textBlockHeight / 2;
+	for (int i = 0; i < rowsCount; i++) 
+	{
+		string longestString = GetLongestString(stringMatrix[i], stringMatrix[i].size());
+		int maxRowHeight = textBlockHeight;
+		for (int j = 0; j < colCount; ++j) 
+		{
+			RECT Block;
+			Block.left = j * TextBlockWidth;
+			Block.right = Block.left + TextBlockWidth;
+			Block.top = totalHeight;
+			Block.bottom = Block.top + textBlockHeight;
+			wstring line = wstring(stringMatrix[i][j].begin(), stringMatrix[i][j].end());
+			int currentBlockHeight = DrawText(hdc, line.c_str(), -1, &Block, DT_WORDBREAK | DT_CENTER | DT_END_ELLIPSIS);
+			maxRowHeight = currentBlockHeight > maxRowHeight ? currentBlockHeight : maxRowHeight;
+		}
+		MoveToEx(hdc, 0, totalHeight - textBlockHeight / 2, nullptr);
+		LineTo(hdc, MainRect.right, totalHeight - textBlockHeight / 2);
+		totalHeight += maxRowHeight + textBlockHeight / 2;
+	}
 }
